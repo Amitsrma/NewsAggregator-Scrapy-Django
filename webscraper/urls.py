bf08cfa095c9b664ca8db829242d5dff1c75ff1a
amit <amit@am>
2023-05-01 00:01:07 -0400

    remove cached files

--- webscraper/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("<int:question_id>", views.displayResult, name = 'displayResult'),
-    path("", views.index, name = 'index'),
-    path("test",views.testDisplay, name = 'testDisplay'),
-]
